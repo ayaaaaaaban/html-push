0304aee8c317f349a62c3bf88987ac8e8628e1b5
LAMIRAULT.E Esteban LAMIRAULT <LAMIRAULT.E@epid-vauban.fr>
2024-03-14 11:18:14 +0100

    Finished adjustements like bad functions and URL

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 function envoyerFormulaire() {
     // Récupération des données du formulaire
-    var nom = document.getElementById('nom').value;
+    var name = document.getElementById('nom').value;
     var email = document.getElementById('email').value;
-    var destination = document.getElementById('destination').value;
     var message = document.getElementById('message').value;
 
     // Création d'un objet FormData pour envoyer les données
-    var formData = new FormData();
-    formData.append('nom', nom);
-    formData.append('email', email);
-    formData.append('destination', destination);
-    formData.append('message', message);
+    var formData = {
+        "name": name,
+        "email": email,
+        "message": message
+    };
+    console.log(formData);
 
     // Envoi des données au serveur (vous devez spécifier l'URL appropriée)
-    fetch('https://m8m.oriatec.fr/webhook/cplr-versionning', {
+    fetch('https://n8n.oriatec.fr/webhook/cplr-versionning', {
         method: 'POST',
-        body: formData
+        body: JSON.stringify(formData),
+        headers: {
+            'Content-Type': 'application/json'
+        }
     })
     .then(response => {
         if (!response.ok) {
